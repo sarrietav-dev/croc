4aaceed0125e95924bb243c38e05d4a769e17546
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2026-07-20 12:31:29 -0500

    fix: reserve helper output for events

--- native/crocbridge/cmd/crocbridge-helper/main.go
+++ native/crocbridge/cmd/crocbridge-helper/main.go
@@ -5,9 +5,12 @@ import (
 	"os"
 
 	"dev.sarrietav/crocbridge/desktopipc"
+	"github.com/schollz/logger"
 )
 
 func main() {
+	logger.SetOutput(os.Stderr)
+	logger.SetLevel("error")
 	if err := desktopipc.Serve(os.Stdin, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
